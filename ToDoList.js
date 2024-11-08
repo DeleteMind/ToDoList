5c9111c1593075997a5196356d77b7701c280b38
DeleteMind <filipe.santos.cordeiro@gmail.com>
2024-11-08 03:15:47 +0000

    add

---
+++ ToDoList.js
@@ -0,0 +1,8 @@
+let inputField = document.getElementById('inputField');
+let toDoContainer = document.getElementById('toDoContainer');
+let addToDoButton = document.getElementById('addToDo');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = documet.createElement('p')
+    toDoContainer.appendChild(paragraph);
+})
